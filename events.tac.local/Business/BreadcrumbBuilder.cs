de6deb471e996e19e360928734008dc72980b2cc
Miguel Ruiz <24594999+devMiguelRuiz@users.noreply.github.com>
2019-09-10 23:27:49 -0500

    Module 5 - Lab: Creating a breadcrumb component

---
+++ events.tac.local/Business/BreadcrumbBuilder.cs
@@ -0,0 +1,35 @@
+﻿using events.tac.local.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TAC.Sitecore.Abstractions.Interfaces;
+using TAC.Sitecore.Abstractions.SitecoreImplementation;
+
+namespace events.tac.local.Business
+{
+    public class BreadcrumbBuilder
+    {
+        private readonly IRenderingContext _context;
+
+        public BreadcrumbBuilder() : this (SitecoreRenderingContext.Create())
+        {
+        }
+
+        public BreadcrumbBuilder(IRenderingContext context)
+        {
+            this._context = context;
+        }
+
+        public IEnumerable<NavigationItem> Build()
+        {
+            if (_context?.HomeItem == null || _context?.ContextItem == null)
+            {
+                return Enumerable.Empty<NavigationItem>();
+            }
+            return _context.ContextItem.GetAncestors()
+                .Where(i => _context.HomeItem.IsAncestorOrSelf(i))
+                .Select(i => new NavigationItem(i.DisplayName, i.Url, false))
+                .Concat(new[] { new NavigationItem (_context.ContextItem.DisplayName, _context.ContextItem.Url, true) });
+        }
+    }
+}
---
+++ events.tac.local/Controllers/BreadcrumbController.cs
@@ -0,0 +1,24 @@
+﻿using events.tac.local.Business;
+using System.Web.Mvc;
+
+namespace events.tac.local.Controllers
+{
+    public class BreadcrumbController : Controller
+    {
+        private readonly BreadcrumbBuilder _builder;
+
+        public BreadcrumbController() : this(new BreadcrumbBuilder())
+        {
+        }
+
+        public BreadcrumbController(BreadcrumbBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_builder.Build());
+        }
+    }
+}
