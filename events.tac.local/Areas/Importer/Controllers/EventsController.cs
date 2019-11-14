077fc7371ca3cc5c0278581ef5046d244ce6e5a2
Miguel Ruiz <24594999+devMiguelRuiz@users.noreply.github.com>
2019-11-13 22:19:08 -0600

    Lab 9.2: Creating User Generated Content Items (From Course of Sitecore 8.2 developer)

---
+++ events.tac.local/Areas/Importer/Controllers/EventsController.cs
@@ -0,0 +1,73 @@
+﻿using events.tac.local.Areas.Importer.Models;
+using Newtonsoft.Json;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.SecurityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace events.tac.local.Areas.Importer.Controllers
+{
+    public class EventsController : Controller
+    {
+        private const string masterDB = "master";
+
+        // GET: Importer/Events
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file, string parentPath)
+        {
+            IEnumerable<Event> events = null;
+
+            using (var reader = new System.IO.StreamReader(file.InputStream))
+            {
+                var content = reader.ReadToEnd();
+
+                try
+                {
+                    events = JsonConvert.DeserializeObject<IEnumerable<Event>>(content);
+
+                    if (events.Any())
+                    {
+                        var eventDetailsTemplateId = new TemplateID(new ID("{1EE92533-8833-4210-818C-7D27E6245462}"));
+
+                        Database database = Sitecore.Configuration.Factory.GetDatabase(masterDB);
+                        Item parentItem = database.GetItem(parentPath);
+
+                        using (new SecurityDisabler())
+                        {
+                            foreach (var @event in events)
+                            {
+                                string name = ItemUtil.ProposeValidItemName(@event.ContentHeading);
+                                Item item = parentItem.Add(name, eventDetailsTemplateId);
+
+                                /* Transaction starts */
+                                item.Editing.BeginEdit();
+                                item["ContentHeading"] = @event.ContentHeading;
+                                item["ContentIntro"] = @event.ContentIntro;
+                                item["Highlights"] = @event.Highlights;
+                                item["StartDate"] = Sitecore.DateUtil.ToIsoDate(@event.StartDate);
+                                item["Duration"] = @event.Duration.ToString();
+                                item["DifficultyLevel"] = @event.Difficulty.ToString();
+                                item.Editing.EndEdit();
+                                /* Transaction ends */
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // to be added later
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ events.tac.local/Areas/Importer/Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace events.tac.local.Areas.Importer.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+
+        public string ContentHeading { get; set; }
+
+        public string ContentIntro { get; set; }
+
+        public string Highlights { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public int Duration { get; set; }
+
+        public int Difficulty { get; set; }
+    }
+}
